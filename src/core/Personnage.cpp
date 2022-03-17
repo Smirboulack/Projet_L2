ba6e292dfdd99c3ea102cb69d7e2da5cb8ee1aae
amark <kramashengqi@gmail.com>
2022-03-17 16:17:45 +0100

    Premier version de Personnage.h et Personnage.cpp

---
+++ src/core/Personnage.cpp
@@ -0,0 +1,105 @@
+#include"Personnage.h"
+#include<iostream>
+
+using namespace std;
+
+Personnage::Personnage(){
+  x = 0;
+  y = 0;
+  vie = 20;
+  sexe = 0;
+  sens = 0;
+  mort = false;
+  nom = "mario";
+  degat = 8;
+  portee = 3;
+}
+
+Personnage::Personnage(int vie, int sexe, string nom, int degat, int portee){
+  x = 0;
+  y = 0;
+  this->vie = vie;
+  this->sexe = sexe;
+  this->nom = nom;
+  this->degat = degat;
+  this->portee = portee;
+}
+
+Personnage::~Personnage(){
+  x = 0;
+  y = 0;
+  vie = 0;
+  mort = true;
+  nom = "";
+  degat = 0;
+  portee = 0;
+}
+void Personnage::deplacer(char direction){
+    if(direction == 'd'){
+      setX(getX()+1);
+    }
+    if(direction == 'g'){
+      setX(getX()-1);
+    }
+    if(direction == 'h'){
+      setY(getY()-1);
+    }
+    if(direction == 'b'){
+      setY(getY()+1);
+    }
+}
+
+void Personnage::setX(int x){
+  this->x = x;
+}
+void Personnage::setY(int y){
+  this->y = y;
+}
+void Personnage::setVie(int vie){
+  this->vie = vie;
+}
+void Personnage::setSexe(int sexe){
+  this->sexe = sexe;
+}
+void Personnage::setSens(int sens){
+  this->sens = sens;
+}
+void Personnage::setMort(bool mort){
+  this->mort = mort;
+}
+void Personnage::setNom(string nom){
+  this->nom = nom;
+}
+void Personnage::setDegat(int degat){
+  this->degat = degat;
+}
+void Personnage::setPortee(int portee){
+  this->portee = portee;
+}
+int Personnage::getX(){
+  return x;
+}
+int Personnage::getY(){
+  return y;
+}
+int Personnage::getVie(){
+  return vie;
+}
+string Personnage::getNom(){
+  return nom;
+}
+int Personnage::getSexe(){
+  return sexe;
+}
+int Personnage::getSens(){
+  return sens;
+}
+bool Personnage::getMort(){
+  return mort;
+}
+int Personnage::getDegat(){
+  return degat;
+}
+int Personnage::getPortee(){
+  return portee;
+}
--- src/core/Personnage.h
+++ src/core/Personnage.h
@@ -2,18 +2,55 @@
 #define PERSONNAGE_H
 
 #include <string>
+using namespace std;
 
-class Personnage 
-{
-    private :
+class Personnage{
+  private:
+    //position du personnage en x et y
     int x, y;
-    string Nom;
+    //point de vie de personnage
+    int vie;
     int sexe;
-    float vie;
+    //le sens du personnage 0 pour gauche et 1 pour droit
+    int sens;
+    //l'etat viant ou mort du personnage
+    bool mort;
+    //le nom du personnage
+    string nom;
+    //dégats du personnage
+    int degat;
+    //portée d'attaque du personnage
+    int portee;
+  public:
+    //crée le personnage par défaut
+    Personnage();
+    //crée le personnage par des attributs
+    Personnage(int vie, int sexe, string nom, int degat, int portee);
+    //déstructeur
+    ~Personnage();
+    //pour déplacer le personnage
+    void deplacer(char direction);
 
-    public :
-    
-}
+    //des getter et setter
+    void setX(int x);
+    void setY(int y);
+    void setVie(int vie);
+    void setSexe(int sexe);
+    void setSens(int sens);
+    void setMort(bool mort);
+    void setNom(string nom);
+    void setDegat(int degat);
+    void setPortee(int portee);
+    int getX();
+    int getY();
+    int getVie();
+    string getNom();
+    int getSexe();
+    int getSens();
+    bool getMort();
+    int getDegat();
+    int getPortee();
+};
 
 
 #endif
